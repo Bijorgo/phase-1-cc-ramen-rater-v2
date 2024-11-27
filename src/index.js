258d90e1e7ba5f8304c109d3cfda1b11f3b1e16a
Ingeborg Adolfs <ingeborgadolfs@gmail.com>
2024-11-26 20:15:53 -0700

    create fetch request, craeate for loop to display each img

--- src/index.js
+++ src/index.js
@@ -13,9 +13,39 @@ const addSubmitListener = document.addEventListener("submit", (event => {
 })); // closes addSubmitListener declaration
 
 const displayRamens = () => {
-  // requests data from server to get all ramen objects
-  // display image for each ramen using and <img> tag inside <div id="ramen-menu">
-  // Add code
+  fetch("http://localhost:3000/ramens") // requests data from server to get all ramen objects, NO ; HERE
+  .then((response) => response.json()) // if connection sucessful, then take info and turn it into json
+  .then((ramenData) => {
+        //console.log(ramenData)
+    //need to create forEach loop
+    const ramens = ramenData//[0].image
+    
+    ramens.forEach(element => {
+          //console.log(element + "How Many?") //5
+
+          // Create -> modify -> append
+      const ramenMenu = document.querySelector("#ramen-menu")
+      const ramenMenuItem = document.createElement("img")
+      
+      //console.log(ramenMenu + "First")
+        //create <img>
+        // now, we want to select "image" and take value, append to <img>
+    ramenMenuItem.src = element.image
+    ramenMenu.append(ramenMenuItem)
+      
+      //ramenMenu.append.ramenMenuItem
+      
+        // display image for each ramen using and <img> tag inside <div id="ramen-menu">
+    });
+        //console.log(ramenImg)
+    // we will need to do something with that data here
+    // traverse objects to find img
+    
+
+  }) //closes .then(ramenData) 
+
+  .catch(error => {console.error(error)});
+  
 }; // closes displayRamens declaration
 
 const main = () => {
@@ -24,14 +54,17 @@ const main = () => {
 } // closes main declaration
 
 main()
+displayRamens();
 
 // Export functions for testing
+/*
 export {
   displayRamens,
   addSubmitListener,
   handleClick,
   main,
 }; // closes export
+*/
 }); // closes DOMContentLoaded event listener 
 
 // new-ramen form to retrieve input info from user is in HTML
